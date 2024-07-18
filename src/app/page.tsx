604e20ca6729a7bfbfe9e727c4129130da1476ae
thebugitself <akhdan909@gmail.com>
2024-07-18 20:57:49 +0700

    icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,6 @@ import Hello from "@/components/Hello";
 import Navbar from "@/components/Navbar";
 import { ParticlesBackground } from "@/components/ParticlesBackground";
 import { useAos } from "@/lib/useAos";
-import { Poppins } from "next/font/google";
-import { Urbanist } from "next/font/google";
-
-const poppins = Poppins({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const urbanist = Urbanist({
-  weight: "600",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export default function Home() {
   useAos();
