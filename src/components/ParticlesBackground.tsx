071ede36bef54deca7d00d7250419e28ecea81e9
thebugitself <akhdan909@gmail.com>
2024-07-18 09:10:48 +0700

    belom bener

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,22 +1,92 @@
-import { loadFull } from "tsparticles"
-import Particles, { initParticlesEngine } from "@tsparticles/react"
-import { useEffect, useMemo, useState } from "react"
-import { particlesOptions } from "./config/particles-config"
+import { loadFull } from "tsparticles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from "react";
 
 export const ParticlesBackground = () => {
-    const [init, setInit] = useState(false)
+  const [init, setInit] = useState(false);
 
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-            await loadFull(engine)
-        }).then(() => {
-            setInit(true)
-        })
-    }, [])
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-    if (init) {
-        return <Particles id="tsparticles" options={particlesOptions} />
-    }
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#000",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: false,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "out",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    );
+  }
 
-    return <></>
-}
+  return <></>;
+};
