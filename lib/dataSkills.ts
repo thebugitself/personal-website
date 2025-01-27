57e5f1fb8bb4a4f4c03730c813b41d6c17fea211
thebugitself <akhdan909@gmail.com>
2025-01-27 10:18:05 +0700

    fix paling

--- lib/dataSkills.ts
+++ lib/dataSkills.ts
@@ -1,85 +1,99 @@
-import { FaNodeJs, FaReact, FaHtml5, FaCss3 } from "react-icons/fa"
-import { SiMongodb, SiTailwindcss, SiNextdotjs, SiTypescript, SiLaravel, SiPrisma, SiPhp, SiGodotengine, SiJavascript, SiPython, SiMysql, SiGithub, SiGit } from 'react-icons/si'
+import { FaNodeJs, FaReact, FaHtml5, FaCss3 } from "react-icons/fa";
+import {
+  SiMongodb,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiTypescript,
+  SiLaravel,
+  SiPrisma,
+  SiPhp,
+  SiGodotengine,
+  SiJavascript,
+  SiPython,
+  SiMysql,
+  SiGithub,
+  SiGit,
+} from "react-icons/si";
 
 export const Skills = [
   {
     id: 1,
-    name: 'React.js',
-    icon: FaReact
+    name: "React.js",
+    icon: FaReact,
   },
   {
     id: 2,
-    name: 'MongoDB',
-    icon: SiMongodb
+    name: "MongoDB",
+    icon: SiMongodb,
   },
   {
     id: 3,
-    name: 'Node.js',
-    icon: FaNodeJs
+    name: "Node.js",
+    icon: FaNodeJs,
   },
   {
     id: 4,
-    name: 'Next.js',
-    icon: SiNextdotjs
+    name: "Next.js",
+    icon: SiNextdotjs,
   },
   {
     id: 5,
-    name: 'Tailwind',
-    icon: SiTailwindcss
+    name: "Tailwind",
+    icon: SiTailwindcss,
   },
   {
     id: 6,
-    name: 'HTML',
-    icon: FaHtml5
+    name: "HTML",
+    icon: FaHtml5,
   },
   {
     id: 7,
-    name: 'CSS',
-    icon: FaCss3
+    name: "CSS",
+    icon: FaCss3,
   },
   {
     id: 8,
-    name: 'Typescript',
-    icon: SiTypescript
+    name: "Typescript",
+    icon: SiTypescript,
   },
   {
     id: 9,
-    name: 'Laravel',
-    icon: SiLaravel
+    name: "Laravel",
+    icon: SiLaravel,
   },
   {
     id: 10,
-    name: 'Prisma',
-    icon: SiPrisma
+    name: "Prisma",
+    icon: SiPrisma,
   },
   {
     id: 11,
-    name: 'PHP',
-    icon: SiPhp
+    name: "PHP",
+    icon: SiPhp,
   },
   {
     id: 12,
-    name: 'Godot',
-    icon: SiGodotengine
+    name: "Godot",
+    icon: SiGodotengine,
   },
   {
     id: 13,
-    name: 'Javascript',
-    icon: SiJavascript
+    name: "Javascript",
+    icon: SiJavascript,
   },
   {
     id: 14,
-    name: 'Python',
-    icon: SiPython
+    name: "Python",
+    icon: SiPython,
   },
   {
     id: 15,
-    name: 'MySQL',
-    icon: SiMysql
+    name: "MySQL",
+    icon: SiMysql,
   },
   {
-    id: 15,
-    name: 'Git',
-    icon: SiGithub
+    id: 16,
+    name: "Git",
+    icon: SiGithub,
   },
-]
+];
