16998fe667611c5978aa7819e919409e36e3f684
thebugitself <akhdan909@gmail.com>
2025-01-27 08:51:39 -0500

    skills ah

--- lib/dataSkills.ts
+++ lib/dataSkills.ts
@@ -1,5 +1,5 @@
 import { FaNodeJs, FaReact, FaHtml5, FaCss3 } from "react-icons/fa"
-import { SiMongodb, SiTailwindcss, SiRedux, SiNextdotjs } from 'react-icons/si'
+import { SiMongodb, SiTailwindcss, SiNextdotjs, SiTypescript, SiLaravel, SiPrisma, SiPhp, SiGodotengine, SiJavascript, SiPython, SiMysql, SiGithub, SiGit } from 'react-icons/si'
 
 export const Skills = [
   {
@@ -29,17 +29,57 @@ export const Skills = [
   },
   {
     id: 6,
-    name: 'Redux',
-    icon: SiRedux
-  },
-  {
-    id: 7,
     name: 'HTML',
     icon: FaHtml5
   },
   {
-    id: 8,
+    id: 7,
     name: 'CSS',
     icon: FaCss3
+  },
+  {
+    id: 8,
+    name: 'Typescript',
+    icon: SiTypescript
+  },
+  {
+    id: 9,
+    name: 'Laravel',
+    icon: SiLaravel
+  },
+  {
+    id: 10,
+    name: 'Prisma',
+    icon: SiPrisma
+  },
+  {
+    id: 11,
+    name: 'PHP',
+    icon: SiPhp
+  },
+  {
+    id: 12,
+    name: 'Godot',
+    icon: SiGodotengine
+  },
+  {
+    id: 13,
+    name: 'Javascript',
+    icon: SiJavascript
+  },
+  {
+    id: 14,
+    name: 'Python',
+    icon: SiPython
+  },
+  {
+    id: 15,
+    name: 'MySQL',
+    icon: SiMysql
+  },
+  {
+    id: 15,
+    name: 'Git',
+    icon: SiGithub
   },
 ]
